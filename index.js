c792db8ab37ad222cf74834b85c61e3fe0b84205
Amir Mahdi Khorshidi <amkgplus@gmail.com>
2019-04-20 20:54:01 +0430

    Fuck Off

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const _ = require('lodash');
 
 const nums = [1, 5, 2, 32, 16, 18, 234, 23];
 
-// nums.forEach((num) => {
-//     console.log(num);
-// })
-
 _.each(nums, (num) => {
    console.log(num);
 });
